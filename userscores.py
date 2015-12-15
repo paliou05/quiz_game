2d128a46ca48272f48c6b61dc805a1c8696ea2b9
Kostas Paliouras <paliou05@gmail.com>
2015-12-15 17:03:15 +0200

    added an unfinished py file

---
+++ userscores.py
@@ -0,0 +1,15 @@
+import pymongo
+import os
+
+
+class UserScores():
+    
+    def __init__(self):
+        self.db = self.connect_to_db('test')
+        
+    def connect_to_db(self,db_name):
+        client = pymongo.MongoClient()
+        return client[db_name]
+    
+    score = 0
+    username = input("Give your username:\n>")
