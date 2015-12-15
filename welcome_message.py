d50e69ee591a21dd8d7e652b3d7808c2668678d7
Kostas Paliouras <paliou05@gmail.com>
2015-12-15 14:25:03 +0200

    added the welcome_message.py

---
+++ welcome_message.py
@@ -0,0 +1,8 @@
+
+print "***********************************"
+print "*             WELCOME             *"
+print "*               TO                *"
+print "*             TECHNO              *"
+print "*              QUIZ               *"
+print "***********************************"
+print raw_input("To Continue press ENTER\n>")
