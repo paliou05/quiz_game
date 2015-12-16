5a4504e11b820070253762bdab8f1c269043f4c0
Kostas Paliouras <paliou05@gmail.com>
2015-12-16 14:12:53 +0200

    under constaction

--- userscores.py
+++ userscores.py
@@ -11,7 +11,24 @@ class UserScores():
         client = pymongo.MongoClient()
         return client[db_name]
     
-    score = 0
-    username = input("Give your username:\n>")
-    #self.db.users.insert({'username':username},{'score':score})
+    def get_username(self):
+        username = input("Give your username:\n>")
+        return username
     
+    def import_userdb(self,username,score):    
+        self.db.users.insert({'username':username},{'score':score})
+    
+    def find_users(self):
+        all_users = self.db.users.find(query,selector)
+        return all_users
+    
+    def print_userscores():
+        """SORT DESCEDING"""
+
+
+"""1)add main.py
+   2)add choice of game(5,10,15,20?)
+   3)print users and scores to txt?
+   4)add the questions"""
+    
+        
